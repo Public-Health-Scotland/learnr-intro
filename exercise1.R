0f88f83cd4a861dac52e02ebae02d0622346b209
James McMahon <james.mcmahon@phs.scot>
2021-08-02 10:44:38 +0100

    Fix quote marks

--- exercise1.R
+++ exercise1.R
@@ -1,5 +1,5 @@
 ## Exercise 1 ----
-# 1. Read in “Borders.csv” (giving the data frame an appropriate name)
+# 1. Read in "Borders.csv" (giving the data frame an appropriate name)
 # 2. What are the mean, median, and max values from the LengthOfStay variable? 
 #     Can you do this in one step?
 # 3. Produce a frequency table to check the sex variable.
